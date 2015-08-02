c0d8f245baf360e5fcec4e979c85fc3a59a9491d
Joaquín Muñoz M <jemunozmunoz@gmail.com>
2015-08-02 20:18:53 -0300

    agregada página de error 503

--- base.py
+++ base.py
@@ -14,6 +14,10 @@ csrf = CsrfProtect(app)
 def page_not_found(e):
     return render_template('errors/404.html'), 404
 
+@app.errorhandler(503):
+def not_authorized(e):
+    return render_template('errors/503.html'), 503
+
 import login
 import voting
 import sudo
